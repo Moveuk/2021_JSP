4e207545b75cbfb5a0420afbef6fde1118aeca4b
Moveuk <84966961+Moveuk@users.noreply.github.com>
2021-08-04 17:12:45 +0900

    Add files via upload

---
+++ 0804_Lecture19/webstudy10_product/product.sql
@@ -0,0 +1,8 @@
+create table product(
+    code number(5),
+    name varchar2(100),
+    price number(8),
+    pictureurl varchar(50),
+    description varchar(1000),
+    primary key(code)
+);
---
+++ 0804_Lecture19/webstudy10_product/src/main/java/com/saeyan/dao/ProductDAO.java
@@ -0,0 +1,52 @@
+package com.saeyan.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.saeyan.dto.ProductVO;
+
+import util.DBManager;
+
+public class ProductDAO {
+
+	private static ProductDAO instance = new ProductDAO();
+	
+	private ProductDAO() {
+		
+	}
+	
+	public static ProductDAO getInstance() {
+		return instance;
+	}
+	
+	public List<ProductVO> selectAllProducts() {
+		String sql = "select * from product order by code desc";
+		List<ProductVO> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) { // 이동은 행(로우) 단위로
+				ProductVO pVo = new ProductVO();
+				pVo.setCode(rs.getInt("code"));
+				pVo.setName(rs.getString("name"));
+				pVo.setPrice(rs.getInt("price"));
+				pVo.setPictureUrl(rs.getString("pictureUrl"));
+				pVo.setDescription(rs.getString("description"));
+				list.add(pVo);
+			}// while 문 끝
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return list;
+	}
+}
